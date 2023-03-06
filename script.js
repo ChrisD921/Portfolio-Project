205ed242656e72f94c955b665331a201b9f1037b
ChrisD921 <cdrape921@gmail.com>
2023-03-06 14:03:43 +0800

    Take out unused JS

--- script.js
+++ script.js
@@ -9,14 +9,6 @@ $('a[href^="#"]').on('click', function(event) {
     }
   });
   
-  // play audio clips
-  $('audio').on('play', function() {
-    $('audio').not(this).each(function() {
-      this.pause();
-      this.currentTime = 0;
-    });
-  });
-  
   // toggle active class on navbar links
   $(window).on('scroll', function() {
     $('section').each(function() {
@@ -26,23 +18,4 @@ $('a[href^="#"]').on('click', function(event) {
         $('nav a[href="#' + id + '"]').addClass('active');
       }
     });
-  });
-  
-  // submit contact form with emailJS
-  $('#contact-form').on('submit', function(event) {
-    event.preventDefault();
-    var form = $(this);
-    var formData = form.serialize();
-    var serviceID = 'default_service';
-    var templateID = 'template_your_template_id_here';
-  
-    form.find('button').text('Sending...');
-    emailjs.send(serviceID, templateID, formData)
-      .then(function() {
-        form.find('button').text('Sent!');
-        form[0].reset();
-      }, function(error) {
-        form.find('button').text('Error!');
-        console.log('FAILED...', error);
-      });
   });
